7fe360dae73a8faa7ac0e3e685afad5ae594ba87
Nick Volkert <nick@nickvolkert.com>
2018-03-12 19:03:26 -0500

    css finally works

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/public/css', express.static(path.join(__dirname + '/public/css')));
+app.use(express.static("app/public"));
 
 require('./app/routing/api-routes.js')(app);
 require('./app/routing/html-routes.js')(app);
