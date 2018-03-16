9c72f568ee1a7236041b6bf16adcef9d3e376a37
Nicholas Volkert <nvolkert@NVs-iMac.attlocal.net>
2018-03-15 22:34:00 -0500

    forgot to add a post command

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -2,5 +2,8 @@ var friendsArray = require ('../data/friends.js');
 module.exports = function(app){
   app.get('/api/friends', function (req, res){
     res.json(friendsArray);
-  })
+  });
+  app.post('/api/friends', function(req, res){
+    //need to loop through and log all of the friends data and push to user if they are a match
+  });
 }
